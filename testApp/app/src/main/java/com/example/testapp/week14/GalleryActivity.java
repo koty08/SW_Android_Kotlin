2f1f00995695f01127a42821365859a574d505c9
Koty <koty08@naver.com>
2022-06-07 19:31:27 +0900

    22-06-07 14week commit

---
+++ testApp/app/src/main/java/com/example/testapp/week14/GalleryActivity.java
@@ -0,0 +1,91 @@
+package com.example.week14ex02;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.util.Locale;
+
+public class GalleryActivity extends AppCompatActivity {
+    public static final String TAG = "[IC]GalleryActivity";
+    public static final int GALLERY_IMAGE_REQUEST_CODE = 1;
+
+    private ClassifierWithModel cls;
+    private ImageView imageView;
+    private TextView textView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+
+        Button selectBtn = findViewById(R.id.selectBtn);
+        selectBtn.setOnClickListener(v-> getImageFromGallery());
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+
+        cls = new ClassifierWithModel(this);
+        try{
+            cls.init();
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+
+    private void getImageFromGallery(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT).setType("image/*");
+
+        startActivityForResult(intent, GALLERY_IMAGE_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && requestCode ==  GALLERY_IMAGE_REQUEST_CODE) {
+            if(data == null) {
+                return;
+            }
+
+            Uri selectedImage = data.getData();
+            Bitmap bitmap = null;
+
+            try {
+                if(Build.VERSION.SDK_INT >= 29) {
+                    ImageDecoder.Source src =
+                            ImageDecoder.createSource(getContentResolver(), selectedImage);
+                    bitmap = ImageDecoder.decodeBitmap(src).copy(Bitmap.Config.ARGB_8888, true);
+                } else {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                }
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to read Image", ioe);
+            }
+
+            if(bitmap != null){
+
+                Pair<String, Float> res = cls.classify(bitmap);
+                String resultStr = String.format(Locale.ENGLISH,
+                        "class: %s, prob: %.1f%%", res.first, res.second * 100.0f );
+
+                textView.setText(resultStr);
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+}
